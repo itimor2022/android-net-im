f50548d5ef56410ee7e6a310a320fade4125c24d
itimor2022 <itimor2022@gmail.com>
2024-07-23 19:34:15 +0700

    add

--- skWeiChatBaidu/src/main/java/com/sk/weichat/AppConfig.java
+++ skWeiChatBaidu/src/main/java/com/sk/weichat/AppConfig.java
@@ -32,16 +32,16 @@ public class AppConfig {
     public static final String apiKey = "";
 
     // 请求地址
-    public static final String APP_URL = "http://im-api.l93lnt5oowndudkio7yw79yv8zj53onk.com/";
-    public static String CONFIG_URL = "http://im-api.l93lnt5oowndudkio7yw79yv8zj53onk.com/config";// Socket 咕喃 服务器入口
+    public static final String APP_URL = "http://im-api.ebay-ukx.com";
+    public static String CONFIG_URL = "http://im-api.ebay-ukx.com/config";// Socket 咕喃 服务器入口
     // public static String CONFIG_URL = "https://imadmin.shijiezhixin.com:8090/config";// Socket 咕喃 服务器入口
 //    public static String CONFIG_URL = "http://1.13.15.121:8090/config";// Socket 咕喃 服务器入口
 
 //图片下载地址
     public static final String IMG_URL = "";
     public static String[] HOST_LIST = {
-            "http://im-api.l93lnt5oowndudkio7yw79yv8zj53onk.com/config",
-            "http://im-api.l93lnt5oowndudkio7yw79yv8zj53onk.com/config"
+            "http://im-api.ebay-ukx.com/config",
+            "http://im-api.ebay-ukx.com/config"
     };
 
     // 发现 - 证书注册
